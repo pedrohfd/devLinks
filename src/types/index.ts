9c9548422c855220fa85d219f80bb0f79e68e449
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-07-10 11:10:45 -0300

    feat: create types files

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface ThemeProps {
+  actualTheme: 'light' | 'dark'
+  theme: {
+    textColor: string
+    strokeColor: string
+    surfaceColor: string
+    strokeColorHover: string
+    surfaceColorHover: string
+    socialLinkHover: string
+    switchColor: string
+    switchBgUrl: string
+  }
+}
